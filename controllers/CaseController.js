02156208141eb91b4b22144b47e22d6fa674dcbb
aliakbarrehman <14bscsarehman@seecs.edu.pk>
2019-04-28 11:59:27 +0500

    Added endpoint for user specific cases using steam ID

--- controllers/CaseController.js
+++ controllers/CaseController.js
@@ -28,6 +28,18 @@ module.exports = function (router) {
                 }))
     });
 
+    // Get cases created by a specific user (steam_id)
+    router.get(`${casesUrl}user/:steam_id`, function (req, res) {
+        Case.find({ steam_id: req.params.steam_id }).exec()
+            .then(docs => res.status(200)
+                .json(docs))
+            .catch(err => res.status(500)
+                .json({
+                    message: 'Error finding cases using steam_id',
+                    error: err
+                }))
+    });
+
     // Create a case
     router.post(casesUrl, function (req, res) {
         let caseObject = new Case(req.body);
