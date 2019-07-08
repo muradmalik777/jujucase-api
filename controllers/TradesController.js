7195d3915c4b37f75465c1af0c0f1ee821cdd6f7
Murad Malik <muradmalik7@gmail.com>
2019-07-08 16:59:17 +0500

    item withdrawl

--- controllers/TradesController.js
+++ controllers/TradesController.js
@@ -1,19 +1,22 @@
 const User = require('../models/User');
 const Winnings = require('../models/Winning');
 const Trade = require('../models/Trade');
+const request = require('request');
 
-const TradeUrl = '/user/trade';
+const TradeUrl = '/user/trade/';
 
 //TODO: get steam_id by jwt and remove from `TradesUrl`
 
 module.exports = function (router) {
-    router.post(TradeUrl, function (req, res) {
+
+    router.post(`${TradeUrl}sell`, function (req, res) {
         Winnings.findOne({ _id: req.body._id, sold: false }).exec().then(winningItem => {
             if(winningItem){
                 User.findOne({ _id: req.body.user_id }).exec().then(userData => {
                     userData.balance = userData.balance + req.body.item.price
                     winningItem.sold = true
                     var tradeObject = new Trade(createTrade(req))
+                    tradeObject.tradeReason = "item sold"
                     tradeObject.save(function(error, tradeData){
                         userData.save(function (error, updatedUser) {
                             winningItem.save(function (error, updatedIWinning) {
@@ -32,6 +35,39 @@ module.exports = function (router) {
                 })
             }
         })
+    });
+
+    router.post(`${TradeUrl}withdraw`, function (req, res) {
+        console.log(req.body.item.marketHashName)
+        const url = "https://api.skingifts.com/orders/v1/purchase/"+ req.body.item.marketHashName +"?apiKey=670706c3-f223-414b-b748-d47dc6f40527"
+        request({ method: 'GET', uri: url }, function (error, response, body) {
+            if (response.statusCode == 200) {
+                var resp = JSON.parse(body)
+                Winnings.findOne({ _id: req.body._id, withdrawn: false, sold: false }).exec().then(winningItem => {
+                    if (winningItem) {
+                        winningItem.withdrawn = true
+                        winningItem.voucherCode = resp.code
+                        winningItem.withdrawlUrl = "https://www.skingifts.com/" + resp.code 
+                        var tradeObject = new Trade(createTrade(req))
+                        tradeObject.tradeReason = "item withdrawn"
+                        tradeObject.save(function (error, tradeData) {
+                            winningItem.save(function (error, updatedWinning) {
+                                res.status(200).json({
+                                    success: true,
+                                    url: updatedWinning.withdrawlUrl
+                                })
+                            })
+                        })
+                    } else {
+                        res.status(420).json({
+                            message: "Item withdrawn already"
+                        })
+                    }
+                })
+            } else {
+                res.status(response.statusCode).json({ message: response.statusMessage })
+            }
+        });
     });
 };
 
--- models/Trade.js
+++ models/Trade.js
@@ -4,6 +4,7 @@ var Schema = mongoose.Schema;
 
 var Trade = new Schema({
     time: { type: Date, default: Date.now },
+    tradeReason: { type: String, default: null },
     item: {
         type: Schema.Types.ObjectId,
         ref: 'Winning'
