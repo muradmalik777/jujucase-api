427981c0bb2108bcdaecab3bbd9fc1c526fcb14f
Murad Malik <muradmalik7@gmail.com>
2019-07-11 16:40:49 +0500

    result verifictaion endpoint

--- controllers/WinningController.js
+++ controllers/WinningController.js
@@ -1,15 +1,17 @@
 let winningUrl = '/winning/';
 const Items = require('../models/Item');
 const Winnings = require('../models/Winning');
+const Case = require('../models/Case');
+const request = require('request');
 
 
 module.exports = function (router) {
 
     // Get all items won by a specific user
     router.post('/user/winning', function (req, res) {
-        Winnings.find({ user_id: req.body.user_id, sold: false, withdrawn: false }, { roundSecret: 0, ticketNumber: 0, clientHash: 0, outcomeId: 0, sold: 0, withdrawn: 0 } ).populate('item').exec().then(unsold => {
-            Winnings.find({ user_id: req.body.user_id, sold: true }, { roundSecret: 0, ticketNumber: 0, clientHash: 0, outcomeId: 0, sold: 0, withdrawn: 0 }).populate('item').exec().then(sold => {
-                Winnings.find({ user_id: req.body.user_id, withdrawn: true }, { roundSecret: 0, ticketNumber: 0, clientHash: 0, outcomeId: 0, sold: 0, withdrawn: 0 }).populate('item').exec().then(withdrawn => {
+        Winnings.find({ user_id: req.body.user_id, sold: false, withdrawn: false }, { sold: 0, withdrawn: 0 } ).populate('item').exec().then(unsold => {
+            Winnings.find({ user_id: req.body.user_id, sold: true }, { sold: 0, withdrawn: 0 }).populate('item').exec().then(sold => {
+                Winnings.find({ user_id: req.body.user_id, withdrawn: true }, { sold: 0, withdrawn: 0 }).populate('item').exec().then(withdrawn => {
                     res.status(200).json({
                         sold_items: sold,
                         unsold_items: unsold,
@@ -20,6 +22,21 @@ module.exports = function (router) {
         })
     });
 
+    router.post('/user/winning/verify', function (req, res) {
+        Case.findOne({ _id: req.body.case_id }).populate('items').exec().then(docs => {
+            var data = inputData(docs.items, req.body.clientHash, req.body.roundSecret)
+            let url = "http://pandorarng.azurewebsites.net/outcomes/v1/verify"
+            request({ method: 'POST', uri: url, json: data }, function (error, response, body) {
+                console.log(body)
+                if (response.statusCode == 200) {
+                    res.status(200).json(body)
+                } else {
+                    res.status(response.statusCode).json("server error")
+                }
+            });
+        })
+    });
+
     // Get all items won from a specific case
     router.post('/case/winning', function (req, res) {
         Winnings.find({ case_id: req.body.case_id }, { roundSecret: 0, ticketNumber: 0, clientHash: 0, outcomeId: 0 }).limit(12).populate('item').exec().then(docs => {
@@ -27,3 +44,25 @@ module.exports = function (router) {
         })
     });
 };
+
+function inputData(items, hash, secret) {
+    let data = {
+        clienHash: hash,
+        roundSecret: secret,
+        drops: dropsData(items)
+    }
+    return data
+}
+
+
+function dropsData(items) {
+    let drops = []
+    items.forEach(item => {
+        let data = {}
+        data.name = item.marketHashName,
+        data.value = item.price
+        data.rate = item.odds / 100
+        drops.push(data)
+    });
+    return drops
+}
